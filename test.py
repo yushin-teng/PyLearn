b51b21365a1038f2c11982b8e628437f21674305
DESKTOP-TFH4IHE\YuShin <tengyushin@gmail.com>
2025-08-19 13:56:55 +0800

    if logical lesson

--- test.py
+++ test.py
@@ -12,8 +12,26 @@
 
 
 #溫度轉換攻勢
-Cdeg = input('請輸入攝氏溫度: ')
-Cdeg = float(Cdeg)
-Fdeg = Cdeg * 9 / 5 + 32
-print('現在華氏溫度為', Fdeg, 'F')
+# Cdeg = input('請輸入攝氏溫度: ')
+# Cdeg = float(Cdeg)
+# Fdeg = Cdeg * 9 / 5 + 32
+# print('現在華氏溫度為', Fdeg, 'F')
 
+#python else語句
+# age = input('請輸入年齡: ')
+# age = int(age)
+# if age >= 20 :
+# 	print('擁有投票資格')
+# else:
+# 	print('回家')
+
+
+#python else if(elif)語句
+age = input('請輸入年齡: ')
+age = int(age)
+if age >= 20 :
+	print('去工作')
+elif age <= 10 and age >= 5:
+	print('小屁孩')
+else:
+	print('小小屁孩')
