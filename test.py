863e5d2a4d634f43926a12816eff28d61cd1e119
DESKTOP-TFH4IHE\YuShin <tengyushin@gmail.com>
2025-08-21 10:11:45 +0800

    while loop logic

--- test.py
+++ test.py
@@ -27,11 +27,40 @@
 
 
 #python else if(elif)語句
-age = input('請輸入年齡: ')
-age = int(age)
-if age >= 20 :
-	print('去工作')
-elif age <= 10 and age >= 5:
-	print('小屁孩')
-else:
-	print('小小屁孩')
+# age = input('請輸入年齡: ')
+# age = int(age)
+# if age >= 20 :
+# 	print('去工作')
+# elif age <= 10 and age >= 5:
+# 	print('小屁孩')
+# else:
+# 	print('小小屁孩')
+
+#loop迴圈 while迴圈
+# x = input('請輸入任意數字: ')
+# x = int(x)
+# while x < 10:
+# 	print('x小於10!')
+# 	print('沒有跳出邏輯')
+# 	x = x + 1
+# print('跳出loop')
+
+#while true迴圈
+# while True:
+# 	print('True一定要寫break')
+# 	break #離開迴圈
+# print('跳脫迴圈')
+
+while True:
+	mode = input('請輸入遊戲模式: ')
+	if mode == 'q':
+		print('遊戲模式一')
+		break
+	elif mode == '1':
+		print('遊戲模式一')
+		break
+	elif mode == '2':
+		print('遊戲模式二')
+		break
+	else:
+		print('只能輸入1/2/q')
