f2bd019ce24f5cdbe05151295291853b721d6438
DESKTOP-TFH4IHE\YuShin <tengyushin@gmail.com>
2025-08-26 17:01:12 +0800

    range 延伸

--- test.py
+++ test.py
@@ -249,3 +249,9 @@ range(3)  # [0, 1, 2]
 for i in range(6):
 	r = random.randint(1, 49)
 	print('這是第',i + 1 ,'次產生隨機數:', r)
+
+range(2, 5) # [2, 3, 4]
+range(8, 10) # [8, 9]
+range(2, 10, 3) # [2, 5, 8]
+range(3, 8, 2) # [3, 5, 7]
+range(10, 3, -2) # [10, 8, 6, 4]
