d88c789bf9527d1a3fd8fd23d9e22bec5bdf565c
DESKTOP-TFH4IHE\YuShin <tengyushin@gmail.com>
2025-08-27 10:57:42 +0800

    Refactor

--- test.py
+++ test.py
@@ -340,9 +340,65 @@
 # result = add(3, 4)
 # print(result)
 
-def average(numbers):
-	return sum(numbers) / len(numbers)
+# def average(numbers):
+# 	return sum(numbers) / len(numbers)
 
-print(average([1, 2, 3]))
+# print(average([1, 2, 3]))
 
+# Refactor(程式重構)
+# #              運算子          變數       清單        篩選條件
+# # output = [(number - 1) for number in reference if num % 2 == 0]
 
+import os
+
+# 讀取檔案
+def read_file(filename):
+		products = []
+		with open(filename, 'r', encoding = 'utf-8') as f:
+			for line in f:
+				if '商品,價格' in line:
+					continue
+				name, price = line.strip().split(',')
+				products.append([name, price])
+			print(products)
+		return products	
+
+# 使用者輸入
+def user_input(products):
+	while True:
+		name = input('輸入購買商品名稱: ')
+		if name == 'q':
+			break
+		else:
+			price = input('輸入購買商品價格:')
+			price = int(price)
+			products.append([name, price])
+	print(products)
+	return products
+
+# 印出所有購買紀錄
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
+
+# 寫入檔案
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+	# 檢查檔案是否存在 (只使用一次，不會重複使用，所以不需要包成函式
+	filename = 'products.txt'
+	if os.path.isfile(filename):	#檢查檔案是否存在再執行
+		print('購物清單存在')
+		products = read_file('products.txt')
+	else:
+		print('該檔案不存在')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.txt', products)
+
+main()
